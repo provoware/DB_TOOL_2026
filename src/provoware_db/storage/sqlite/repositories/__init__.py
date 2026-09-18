d5d9d3db8f78a5043927f14ec6526800f873e9ca
pppoppi <provoware.one@gmail.com>
2026-09-18 19:09:05 +0200

    import: add src/provoware_db/storage/sqlite/repositories/__init__.py

---
+++ src/provoware_db/storage/sqlite/repositories/__init__.py
@@ -0,0 +1,5 @@
+from .category_repository import CategoryRepository
+from .entry_repository import EntryRepository
+from .field_repository import FieldRepository
+
+__all__ = ["CategoryRepository", "EntryRepository", "FieldRepository"]
